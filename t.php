a439560e5eaabd49e5b58529ae806b78abefcfdb
msa2022ai <124654029+msa2022ai@users.noreply.github.com>
2023-02-07 13:09:22 +0800

    Add files via upload

---
+++ t.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<textarea autocomplete="off" id="textareaCode" wrap="logical" spellcheck="false" style="display: none;">&lt;!DOCTYPE HTML&gt;
+&lt;html&gt;
+&lt;body&gt;
+
+&lt;video width="320" height="240" controls="controls"&gt;
+  &lt;source src="/i/movie.ogg" type="video/ogg"&gt;
+  &lt;source src="/i/movie.mp4" type="video/mp4"&gt;
+Your browser does not support the video tag.
+&lt;/video&gt;
+
+&lt;/body&gt;
+&lt;/html&gt;
+</textarea>
+</body>
+</html>
+
